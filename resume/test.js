3d3d2832a2532b3f39453159e2ddc435033b3b02
Mozarel94 <40889429+Mozarel94@users.noreply.github.com>
2018-11-12 10:00:37 +0300

    Резюме 12 ноября

--- resume/test.js
+++ resume/test.js
@@ -2,7 +2,7 @@ menu.onclick = function myFunction() {
   let x = document.getElementById("myTopnav");
 
   if(x.className === "topnav") {
-    x.className += "responsive";
+    x.className += " responsive";
   } else {
     x.className = "topnav";
   }
